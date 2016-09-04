da853ea63badba5f15ec03440e1ce9ee2001189a
KhaledaNYC <khaleda.khan87@gmail.com>
2016-09-04 18:29:10 -0400

    set up generic resources routes root is at boards index

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/show'
-
-  get 'lists/index'
-
-  get 'lists/new'
-
-  get 'lists/create'
-
-  get 'lists/show'
-
-  get 'boards/index'
-
-  get 'boards/new'
-
-  get 'boards/create'
-
-  get 'boards/show'
-
+ resources :boards
+ resources :lists
+ resources :tasks
+  root 'boards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
