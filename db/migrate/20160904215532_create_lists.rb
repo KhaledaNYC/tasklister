56272fb7dc7c1ec0fed13dc955d961f2f3433fd9
KhaledaNYC <khaleda.khan87@gmail.com>
2016-09-04 18:11:46 -0400

    tables migrated

--- db/migrate/20160904215532_create_lists.rb
+++ db/migrate/20160904215532_create_lists.rb
@@ -1,7 +1,8 @@
 class CreateLists < ActiveRecord::Migration[5.0]
   def change
     create_table :lists do |t|
-
+      t.string :name
+      t.belongs_to :board, index: true, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20160904215537_create_tasks.rb
+++ db/migrate/20160904215537_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-
+      t.string :name
+      t.belongs_to :list, index: true, foreign_key: true
       t.timestamps
     end
   end
